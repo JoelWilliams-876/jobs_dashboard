c269765f3defe564677ddb11b9b1edf1c50b29e0
Joel Williams <jwilliams3@unomaha.edu>
2024-12-11 15:51:03 -0600

    Update jobs_dashboard.py

--- jobs_dashboard.py
+++ jobs_dashboard.py
@@ -13,7 +13,7 @@ import pandas as pd
 import plotly.express as px
 from bs4 import BeautifulSoup
 import streamlit as st
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler as bs
 from datetime import datetime
 import holidays
 
@@ -95,7 +95,7 @@ if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame()
 
 # Start APScheduler in the background to fetch data on the first business day of each month
-scheduler = BackgroundScheduler()
+scheduler = bs()
 scheduler.add_job(fetch_and_update_data, 'cron', day='1-7', hour=9, minute=0)  # Check daily at midnight
 scheduler.start()
 
